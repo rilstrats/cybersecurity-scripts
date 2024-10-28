7da87867b1f4e79013098e9e0eda8485ef01e4b2
Riley <91571914+rilstrats@users.noreply.github.com>
2024-10-28 16:02:15 -0600

    updated test.sh to automatically prepare for tests

--- test.sh
+++ test.sh
@@ -3,3 +3,8 @@ sudo useradd badsudouser # add this user to /etc/sudoers
 sudo useradd badsudousergroup
 sudo useradd badsudogroup # add this group to /etc/sudoers
 sudo usermod -aG sudo badsudousergroup
+sudo cp /etc/sudoers /tmp/sudoers
+echo  "\nbadsudouser\tALL=(ALL:ALL)\tALL\n%badsudogroup\tALL=(ALL:ALL)\tALL\n" | sudo tee -a /tmp/sudoers > /dev/null
+sudo visudo -c -f /tmp/sudoers > /dev/null && \
+	sudo cp /tmp/sudoers /etc/sudoers
+sudo rm /tmp/sudoers
